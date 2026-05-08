9c6d315994139d773271aec44f1680456fa761b2
thestackersudo <reset39594765@gmail.com>
2026-05-08 10:13:57 +0100

    Initial Create

---
+++ main.lua
@@ -0,0 +1,26 @@
+function pPrint(text)
+	print("[Plink]\t" .. text)
+end
+
+function Load() 
+	local switch = {
+	["92416421522960"] = function() 
+		pPrint("Slime RNG Found.")
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/thestackersudo/SRNG/main/main.lua"))()
+	end,
+	["95630541662383"] = function()
+		pPrint("World Fighters Found.")
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/thestackersudo/WF/main/main.lua"))()
+	end,
+	default = function()
+		pPrint("Game not Found.")
+	end
+}
+	if switch[tostring(game.PlaceId)] then
+		switch[tostring(game.PlaceId)]()
+	else
+		switch["default"]()
+	end
+end
+
+Load()
